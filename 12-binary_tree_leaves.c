bff0769e7cc142ce10f9e9154bc29e292f7e5fcf
Mubarek Seid <mubareksd@gmail.com>
2022-11-16 23:20:06 +0300

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,8 +11,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (1);
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (((!tree->left && !tree->right) ? 1 : 0) +
+			binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right));
 }
